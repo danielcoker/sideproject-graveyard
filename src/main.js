b4d8496c95f3686ab12f3388a2b1683458044532
Daniel Coker <36392491+danielcoker@users.noreply.github.com>
2020-08-19 01:24:34 +0100

    Install and setup firebase

--- src/main.js
+++ src/main.js
@@ -1,9 +1,25 @@
 import Vue from 'vue';
+import firebase from 'firebase/app';
+import 'firebase/auth';
 import vSelect from 'vue-select';
 import App from './App.vue';
 import router from './router';
 
 Vue.config.productionTip = false;
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyCIO4cuTXxpZPxmkOJbevghAxPTpW9pkVo',
+  authDomain: 'sideproject-dced6.firebaseapp.com',
+  databaseURL: 'https://sideproject-dced6.firebaseio.com',
+  projectId: 'sideproject-dced6',
+  storageBucket: 'sideproject-dced6.appspot.com',
+  messagingSenderId: '108597942219',
+  appId: '1:108597942219:web:b4750eff9c64c4a50a484e',
+  measurementId: 'G-Z24WKNL0K4',
+};
+
+firebase.initializeApp(firebaseConfig);
+// firebase.analytics();
 
 Vue.component('v-select', vSelect);
 
