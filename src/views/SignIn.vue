5a03ed86fa921bbcdae744119dcc2e97abc19c63
Daniel Coker <36392491+danielcoker@users.noreply.github.com>
2020-08-19 01:25:03 +0100

    Add signin with GitHub

--- src/views/SignIn.vue
+++ src/views/SignIn.vue
@@ -4,13 +4,14 @@
 
     <main class="pt-64 pb-48 w-11/12 ml-auto mr-auto mb-10 lg:py-64">
       <section class="w-full md:w-1/3 m-auto">
-        <router-link
-          :to="{ name: 'Home' }"
+        <a
           class="my-2 flex justify-center space-x-5 lg:my-0 mx-2 btn py-4"
+          href="#"
+          @click.prevent="signInWithGitHub"
         >
           <img src="../assets/github.svg" alt="GitHub" />
           <p>Sign in with GitHub</p>
-        </router-link>
+        </a>
       </section>
     </main>
 
@@ -19,6 +20,7 @@
 </template>
 
 <script>
+import firebase from 'firebase';
 import Header from '@/components/Header.vue';
 import Footer from '@/components/Footer.vue';
 
@@ -27,6 +29,32 @@ export default {
   components: {
     Header,
     Footer,
+  },
+  methods: {
+    signInWithGitHub() {
+      const provider = new firebase.auth.GithubAuthProvider();
+      provider.addScope('repo');
+
+      firebase
+        .auth()
+        .signInWithPopup(provider)
+        .then((result) => {
+          const token = result.credential.accessToken;
+          const { user } = result;
+
+          user.token = token;
+
+          window.localStorage.setItem('user', JSON.stringify(user));
+          this.$router.push({ name: 'Dashboard' });
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+
+          // eslint-disable-next-line no-console
+          console.log(errorCode, errorMessage);
+        });
+    },
   },
 };
 </script>
