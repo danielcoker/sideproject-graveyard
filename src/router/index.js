a441848132c972b7d8c7e05933ce6186f61ec17b
Daniel Coker <36392491+danielcoker@users.noreply.github.com>
2020-09-01 00:58:17 +0100

    Fix scroll position issue

--- src/router/index.js
+++ src/router/index.js
@@ -51,6 +51,10 @@ const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes,
+  // eslint-disable-next-line no-unused-vars
+  scrollBehavior(to, from, savedPosition) {
+    return { x: 0, y: 0 };
+  },
 });
 
 export default router;
