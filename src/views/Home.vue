6f397b88861547b97e4855a9e315a97aecd080ae
Daniel Coker <36392491+danielcoker@users.noreply.github.com>
2020-08-31 02:07:41 +0100

    Add project listing to home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,7 +5,7 @@
     <Hero />
 
     <main class="w-11/12 ml-auto mr-auto mt-10 mb-10">
-      <Projects />
+      <Projects :projects="projects" :busy="busy" @loadmore="loadMore" />
     </main>
 
     <Footer />
@@ -13,6 +13,7 @@
 </template>
 
 <script>
+import firebase from 'firebase';
 import Header from '@/components/Header.vue';
 import Hero from '@/components/Hero.vue';
 import Projects from '@/components/Projects.vue';
@@ -28,13 +29,95 @@ export default {
   },
   data() {
     return {
-      hidden: true,
+      projects: [],
+      lastVisible: null,
+      busy: true,
     };
   },
   methods: {
-    toggleNavContent() {
-      document.getElementById('nav-content').classList.toggle('hidden');
+    getUser(project) {
+      const userRef = firebase
+        .firestore()
+        .collection('users')
+        .doc(project.user);
+
+      userRef
+        .get()
+        .then((userDoc) => {
+          if (userDoc.exists) {
+            const user = {
+              displayName: userDoc.data().displayName,
+              photoURL: userDoc.data().photoURL,
+            };
+
+            // eslint-disable-next-line no-param-reassign
+            project.user = user;
+
+            // eslint-disable-next-line no-param-reassign
+            [project.user.username] = project.repo.split('/');
+          }
+        })
+        .catch(() => {});
     },
+    loadMore() {
+      const vm = this;
+
+      if (vm.lastVisible) {
+        vm.busy = true;
+
+        const nextProjects = firebase
+          .firestore()
+          .collection('projects')
+          .orderBy('createdAt', 'desc')
+          .startAfter(vm.lastVisible)
+          .limit(10);
+
+        nextProjects.get().then((documentSnapshots) => {
+          let currentProjects = documentSnapshots.docs.map((document) => ({
+            ...document.data(),
+            id: document.id,
+          }));
+
+          currentProjects = currentProjects.map((project) => {
+            vm.getUser(project);
+
+            return project;
+          });
+
+          vm.projects = [...vm.projects, ...currentProjects];
+          vm.lastVisible = documentSnapshots.docs[documentSnapshots.docs.length - 1];
+
+          vm.busy = false;
+        });
+      }
+    },
+  },
+  mounted() {
+    const vm = this;
+
+    const projects = firebase
+      .firestore()
+      .collection('projects')
+      .orderBy('createdAt', 'desc')
+      .limit(10);
+
+    projects.get().then((documentSnapshots) => {
+      const currentProjects = documentSnapshots.docs.map((document) => ({
+        ...document.data(),
+        id: document.id,
+      }));
+
+      vm.projects = [...currentProjects];
+      vm.lastVisible = documentSnapshots.docs[documentSnapshots.docs.length - 1];
+
+      vm.projects = vm.projects.map((project) => {
+        vm.getUser(project);
+
+        return project;
+      });
+
+      vm.busy = false;
+    });
   },
 };
 </script>
